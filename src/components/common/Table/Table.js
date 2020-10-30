78c1d8a7cacb50c18e50a0d14d0e4e3bd3cbee4f
Josh Blaha <1719239+che-wf@users.noreply.github.com>
2020-10-29 19:33:54 -0600

    Refactor Table

--- src/components/common/Table/Table.js
+++ src/components/common/Table/Table.js
@@ -4,14 +4,29 @@ import { Cell } from './Cell';
 import './Table.css';
 
 export function Table({ columns, data }) {
-  const header = useMemo(
+  const header = useMakeHeader(columns);
+  const tableContents = useMakeTableContents(data, columns);
+
+  return (
+    <table className="Table">
+      <thead>{header}</thead>
+      <tbody>{tableContents}</tbody>
+    </table>
+  );
+}
+
+function useMakeHeader(columns) {
+  const HeaderRow = useMemo(
     () =>
       columns.map((column, index) => (
         <Cell key={`${column.title}-header-${index}`}>{column.title}</Cell>
       )),
     [columns]
   );
+  return <tr>{HeaderRow}</tr>;
+}
 
+function useMakeTableContents(data, columns) {
   const tableContents = useMemo(
     () =>
       data.map((datum) => {
@@ -30,12 +45,5 @@ export function Table({ columns, data }) {
     return 'There are currently no results.';
   }
 
-  return (
-    <table className="Table">
-      <thead>
-        <tr>{header}</tr>
-      </thead>
-      <tbody>{tableContents}</tbody>
-    </table>
-  );
+  return tableContents;
 }
